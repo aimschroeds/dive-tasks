73e67891151e00005f233a459b4c3093a4a6f2b7
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2022-12-23 19:09:55 +0100

    Added navigation between existing screens and auto redirect of logged in users

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { auth } from "./firebase";
+import { registerRootComponent } from "expo";
+
+import LoginView from "./views/LoginView";
+import RegistrationView from "./views/RegistrationView";
+import ResetPasswordView from "./views/ResetPasswordView";
+import NavigationMain from "./navigation/NavigationMain";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group>
+            <Stack.Screen
+              name="NavigationMain"
+              component={NavigationMain}
+              options={{ headerShown: false }}
+            />
+          <Stack.Screen
+            name="Login"
+            component={LoginView}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Registration"
+            component={RegistrationView}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Reset Password"
+            component={ResetPasswordView}
+            options={{ headerShown: false }}
+          />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+registerRootComponent(App);
---
+++ navigation/NavigationMain.js
@@ -0,0 +1,39 @@
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import HomeView from '../views/HomeView';
+
+import { auth } from '../firebase';
+
+const Tab = createMaterialBottomTabNavigator();
+
+/**
+ * Primary bottom tab navigation for the app (logged in users only)
+ * @returns {JSX.Element} 
+ */
+
+function NavigationMain() {
+    return (
+    <Tab.Navigator
+    initialRouteName='Home'
+    activeColor="#C9EEFF"
+    inactiveColor="#3e2465"
+    barStyle={{ backgroundColor: '#AA77FF' }}
+    >
+      <Tab.Screen
+          name='Home'
+          component={HomeView}
+            options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+                ),
+            }}
+      />    
+    </Tab.Navigator>
+    );}
+
+export default NavigationMain;

8eca52112ed73c8df2655b6fa4b88d6fcafb5d05
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-01-12 20:12:09 +0100

    AddPlan Screen and Components

--- views/PlanAddView.js
+++ views/PlanAddView.js
@@ -1,45 +1,125 @@
 import Styles from "../styles/Styles";
-import { navigation } from "@react-navigation/native";
-import { KeyboardAvoidingView, View, Text, TextInput } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  KeyboardAvoidingView,
+  RefreshControl,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import { Cell, Section, TableView } from "react-native-tableview-simple";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import MilestoneAdd from "../components/MilestoneAdd";
 
-const PlanAddView = ({ navigation }) => {
-    const [plan, setPlan] = useState({
-        title: "",
-        complete: false,
-        milestones: [],
+const PlanAddView = () => {
+  const navigation = useNavigation();
+  const [plan, setPlan] = useState({
+    title: "",
+    status: false,
+    milestones: [],
+  });
+
+  console.log(plan);
+
+  const setTitle = (text) => {
+    setPlan({ ...plan, title: text });
+  };
+
+  const addMilestone = () => {
+    setPlan({
+      ...plan,
+      milestones: [
+        ...plan.milestones,
+        { name: "", status: false, type: "", skills: [] },
+      ],
     });
+  };
 
-    setTitle = (text) => {
-        setPlan({ ...plan, title: text });
-    };
-
-    return (
-    <KeyboardAvoidingView style={Styles.container}>
-        <TableView hideSeparator> 
-            <Section hideSeparator hideSurroundingSeparators>
-                <Cell
-                    cellAccessoryView={
-                        <TextInput
-                            placeholder="Certification You Are Pursuing"
-                            // style={Styles.input}
-                            value={plan.title}
-                            onChangeText={(text) => setTitle(text)}
-                            autoFocus={true}
-                            autoCapitalize="words"
-                        ></TextInput>
-                    }
-                    title="Goal"
-                    titleTextColor={"purple"}
-                    titleTextStyle={Styles.cellTitleText}
-                    hideSeparator={true}
-                    cellStyle="RightDetail"
-                >
-                </Cell>
-                </Section>
-        </TableView>
-    </KeyboardAvoidingView>
+  return (
+    <SafeAreaView style={{ backgroundColor: "white" }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={160}
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          refreshControl={
+            <RefreshControl refreshing={false} />
+          }
+        >
+          <TableView hideSeparator>
+            <Section
+              hideSeparator
+              sectionPaddingBottom={5}
+              sectionPaddingTop={10}
+              header="Add Plan"
+              headerTextStyle={Styles.tableHeader}
+            >
+              <Cell
+                cellContentView={
+                  <View>
+                    <TextInput
+                      style={Styles.textInput}
+                      placeholder="Certification"
+                      value={plan.title}
+                      onChangeText={(text) => setTitle(text)}
+                      autoFocus={true}
+                      autoCapitalize="words"
+                    ></TextInput>
+                  </View>
+                }
+                hideSeparator={true}
+              ></Cell>
+            </Section>
+            {/* Add a MilestoneAdd component for each milestone in the plan */}
+            {plan.milestones.map((milestone, index) => {
+              return (
+                <MilestoneAdd
+                  key={index}
+                  milestone={milestone}
+                  setMilestone={(milestone) => {
+                    // Create a new array of milestones, replacing the one at the current index with the new milestone
+                    const newMilestones = [...plan.milestones];
+                    newMilestones[index] = milestone;
+                    // Update the plan with the new array of milestones
+                    setPlan({ ...plan, milestones: newMilestones });
+                  }}
+                  deleteMilestone={() => {
+                    // Create a new array of milestones, removing the one at the current index
+                    const newMilestones = [...plan.milestones];
+                    newMilestones.splice(index, 1);
+                    // Update the plan with the new array of milestones
+                    setPlan({ ...plan, milestones: newMilestones });
+                  }}
+                  index={index}
+                />
+              );
+            })}
+          </TableView>
+            {/* Add a button to add another milestone */}
+            <TouchableOpacity
+                onPress={() => {
+                    addMilestone();
+                }}
+                style={Styles.buttonSecondary}
+            >
+                <Text style={[Styles.buttonText, { color: "white", margin: 10 }]}>Add Another Milestone</Text>
+            </TouchableOpacity>
+            {/* Add a button which to save plan */}
+            <TouchableOpacity
+                onPress={() => {
+                    addPlan();
+                }}
+                style={[Styles.buttonPlan, {  }]}
+            >
+                <Text style={[Styles.buttonText, { color: "white", margin: 10 }]}>Save Plan</Text>
+            </TouchableOpacity>
+            </ScrollView>
+        </KeyboardAvoidingView>
+    </SafeAreaView>
     )}
 
 export default PlanAddView;

6e471e18aadaa9ab696b9720b24f111315633655
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-02-12 12:43:02 +0100

    Added image modal and fixed bug related to slider left, right arrows

--- components/Images.js
+++ components/Images.js
@@ -1,27 +1,42 @@
-import React, { useRef } from "react";
-import { View, Image, ScrollView, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { Modal, View, Image, ScrollView, TouchableOpacity } from "react-native";
 import Styles from "../styles/Styles";
 import { deleteImage } from "../utils/deleteImage";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Images = ({ entityId, entityPath, images }) => {
+const Images = ({ entityId, entityPath, images, refreshImages }) => {
   const scrollViewRef = useRef();
+  const [contentOffset, setContentOffset] = useState({ x: 0, y: 0 });
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalImage, setModalImage] = useState(null); 
 
   const handleDeleteImage = async (image) => {
     try {
       await deleteImage(entityId, entityPath, image);
+      // Refresh the images
+        refreshImages();
     } catch (error) {
       console.error("Error deleting image:", error);
     }
   };
 
+  const handleModalOpen = (image) => {
+    setModalImage(image);
+    setModalVisible(true);
+    };
+
+    const handleModalClose = () => {
+    setModalImage(null);
+    setModalVisible(false);
+    };
+
   return (
     <View style={Styles.imageScrollContainer}>
-      <TouchableOpacity
+      {images.length >2 && <TouchableOpacity
         // When pressed scroll to the left a small amount
         onPress={() => {
           scrollViewRef.current.scrollTo({
-            x: scrollViewRef.current.contentOffset.x - 100,
+            x: contentOffset.x - 100,
             animated: true,
           });
         }}
@@ -32,15 +47,22 @@ const Images = ({ entityId, entityPath, images }) => {
           size={24}
           color="white"
         />
-      </TouchableOpacity>
+      </TouchableOpacity>}
       <ScrollView
         horizontal={true}
         style={Styles.imageScroll}
         contentContainerStyle={{ alignItems: "center" }}
         ref={scrollViewRef}
+        onScroll={(event) => {
+            setContentOffset(event.nativeEvent.contentOffset);
+        }}
+        scrollEventThrottle={16}
       >
         {images.map((image, index) => (
           <View key={index} style={Styles.imageWrapper}>
+            <TouchableOpacity
+                onPress={() => handleModalOpen(image)}
+            >
             <Image source={{ uri: image }} style={Styles.image} />
             <TouchableOpacity
               style={Styles.imageDeleteButton}
@@ -48,14 +70,15 @@ const Images = ({ entityId, entityPath, images }) => {
             >
               <MaterialCommunityIcons name="delete" size={24} color="white" />
             </TouchableOpacity>
+            </TouchableOpacity>
           </View>
         ))}
       </ScrollView>
-      <TouchableOpacity
+      {images.length > 2 && <TouchableOpacity
         // When pressed scroll to the left a small amount
         onPress={() => {
           scrollViewRef.current.scrollTo({
-            x: scrollViewRef.current.contentOffset.x + 100,
+            x: contentOffset.x + 100,
             animated: true,
           });
         }}
@@ -66,7 +89,25 @@ const Images = ({ entityId, entityPath, images }) => {
           size={24}
           color="white"
         />
-      </TouchableOpacity>
+      </TouchableOpacity>}
+      <Modal
+        visible={modalVisible}
+        onRequestClose={handleModalClose}
+        animationType="fade"
+        transparent={true}
+        >
+            <TouchableOpacity
+            style={Styles.modalContainer}
+            onPress={handleModalClose}
+            activeOpacity={1}
+            >
+                <Image source={{ uri: modalImage }} 
+                style={Styles.modalImage} 
+                resizeMode="contain"
+                />
+            </TouchableOpacity>
+        </Modal>
+
     </View>
   );
 };

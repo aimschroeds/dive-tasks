8485d5b051754e16a1f69c46ce520bb587bcf61c
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-02-25 18:37:20 +0100

    Refactored imagePicker so it can be used for single or multiple images

--- hooks/useImagePicker.js
+++ hooks/useImagePicker.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 
-const useImagePicker = () => {
+const useImagePicker = (options) => {
   const [selectedImages, setSelectedImages] = useState([]);
 
   const pickImages = async () => {
@@ -13,16 +13,13 @@ const useImagePicker = () => {
 
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsMultipleSelection: true,
-      maxImagesCount: 10,
-      quality: 0.5,
+      ...options,
     });
 
     if (!result.canceled) {
-      setSelectedImages(result.assets);
+      setSelectedImages(result.assets || [result]);
     }
   };
-
   return { selectedImages, pickImages };
 };
 

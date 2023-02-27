13f5cf0df8ae3eb2b39bcf71eaf82c043d39d124
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-02-27 21:32:17 +0100

    Deal with login state on home page

--- views/HomeView.js
+++ views/HomeView.js
@@ -2,13 +2,34 @@ import { Text, View, TextInput } from 'react-native';
 import Styles from '../styles/Styles';
 
 import Logout from '../components/Logout';
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
 /**
  * Home screen
  * @param {*} navigation
  * @returns {JSX.Element}
  */
 
-const HomeView = ({ navigation }) => {
+const HomeView = () => {
+    const navigation = useNavigation();
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        // Listen for auth state changes
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in
+                setUserId(user.uid);
+            } else {
+                // User is signed out
+                setUserId(null);
+                navigation.navigate('Login');
+            }
+        })
+        return unsubscribe;
+    }, [navigation]);
 
     return (
         <View style={Styles.container}>

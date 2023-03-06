a9557daa494d52bba134b19025c04fbcff072350
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-03-06 08:28:05 +0100

    Improved commenting + added notifications

--- views/OtherProfileView.js
+++ views/OtherProfileView.js
@@ -3,11 +3,16 @@ import { Image, Text, View, StyleSheet, ActivityIndicator, TouchableOpacity } fr
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { db, auth } from "../firebase";
-import { collection, doc, getDoc, getDocs, query, where, setDoc, addDoc, deleteDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, query, where, setDoc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import Styles from '../styles/Styles';
+import { sendFriendshipAcceptanceNotification, sendFriendshipRequestNotification } from "../helpers/notificationHelpers.js";
 
+/**
+ * OtherProfileView component shows another user's profile and allows the current user
+ * to interact with them (e.g. send/accept friend requests).
+ */
 const OtherProfileView = ({ route, navigation }) => {
   const { viewingUserId } = route.params;
   const [relationshipStatus, setRelationshipStatus] = useState(null);
@@ -16,25 +21,23 @@ const OtherProfileView = ({ route, navigation }) => {
   const [loading, setLoading] = useState(true);
   const [currentUserId, setCurrentUserId] = useState();
   const [sentRequest, setSentRequest] = useState(false);
+  const [requesterId, setRequesterId] = useState(null);
 
-    useEffect(() => {
-        // Listen for auth state changes
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User is signed in
-                setCurrentUserId(user.uid);
-                console.log('user.uid:', user.uid)
-            } else {
-                // User is signed out
-                setCurrentUserId(null);
-                navigation.navigate('Login');
-            }
-        })
-        return unsubscribe;
-    }, [navigation]);
-
+  // Set up a listener for the current user's authentication state
   useEffect(() => {
-    console.log('useEffect viewingUserID:', viewingUserId)
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUserId(user.uid);
+      } else {
+        setCurrentUserId(null);
+        navigation.navigate('Login');
+      }
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  // Fetch data for the user being viewed
+  useEffect(() => {
     const fetchUser = async () => {
       const docRef = doc(db, "users", viewingUserId);
       const docSnap = await getDoc(docRef);
@@ -46,36 +49,42 @@ const OtherProfileView = ({ route, navigation }) => {
     fetchUser();
   }, [viewingUserId]);
 
+  // Fetch the current relationship status between the current user and the user being viewed
   const fetchRelationshipStatus = async () => {
     const relationshipRef = collection(db, "relationships");
-  
+
+    // Query for relationships where the current user is userId1
     const q1 = query(
       relationshipRef,
       where("userId1", "==", currentUserId),
       where("userId2", "==", viewingUserId)
     );
-  
+
+    // Query for relationships where the current user is userId2
     const q2 = query(
       relationshipRef,
       where("userId1", "==", viewingUserId),
       where("userId2", "==", currentUserId)
     );
-  
+
     const querySnapshot1 = await getDocs(q1);
     const querySnapshot2 = await getDocs(q2);
     let relationshipDoc;
-  
+
+    // Determine the correct relationship document based on the query results
     if (!querySnapshot1.empty) {
       relationshipDoc = querySnapshot1.docs[0];
     } else if (!querySnapshot2.empty) {
       relationshipDoc = querySnapshot2.docs[0];
     }
-  
+
+    // Set relationship state variables based on the relationship document
     if (relationshipDoc) {
       setRelationshipDocId(relationshipDoc.id);
       const data = relationshipDoc.data();
       const status =
         data.userId1 === currentUserId ? data.status1 : data.status2;
+      setRequesterId(data.userId1);
       setRelationshipStatus(status);
     } else {
       setRelationshipStatus("not friends");
@@ -83,7 +92,7 @@ const OtherProfileView = ({ route, navigation }) => {
   };
   
   
-  
+  // Fetch relationship status whenever the current user or user being viewed changes
   useEffect(() => {
     if (currentUserId && viewingUserId)
     {
@@ -91,6 +100,7 @@ const OtherProfileView = ({ route, navigation }) => {
     }
   }, [viewingUserId, currentUserId]);
   
+  // Send a friend request from the current user to the user being viewed
   const sendFriendRequest = async () => {
     await addDoc(collection(db, "relationships"), {
       userId1: currentUserId,
@@ -100,29 +110,38 @@ const OtherProfileView = ({ route, navigation }) => {
     });
     setRelationshipStatus("pending");
     setSentRequest(true);
+    // Send a notification for the friendship request
+    await sendFriendshipRequestNotification(currentUserId, viewingUserId);
   };
-  
+
+  // Cancel a friend request sent by the current user
   const cancelFriendRequest = async () => {
     await deleteDoc(doc(db, "relationships", relationshipDocId));
     setRelationshipStatus("not friends");
   };
-  
+
+  // Accept a friend request received by the current user
   const acceptFriendRequest = async () => {
     await updateDoc(doc(db, "relationships", relationshipDocId), {
-      [`status${currentUserId === userId1 ? 1 : 2}`]: "friends",
+      [`status${currentUserId === requesterId ? 2 : 1}`]: "friends",
     });
     setRelationshipStatus("friends");
+    // Send a notification for the friendship acceptance
+    await sendFriendshipAcceptanceNotification(currentUserId, requesterId);
   };
-  
+
+  // Decline a friend request received by the current user
   const declineFriendRequest = async () => {
     await deleteDoc(doc(db, "relationships", relationshipDocId));
     setRelationshipStatus("not friends");
   };
-  
+
+  // Unfriend the user being viewed
   const unfriend = async () => {
     await deleteDoc(doc(db, "relationships", relationshipDocId));
     setRelationshipStatus("not friends");
   };
+
   
   
 
@@ -175,22 +194,27 @@ const OtherProfileView = ({ route, navigation }) => {
             </TouchableOpacity>
             ) : (
             <View>
-                <TouchableOpacity
-                style={styles.actionButton}
-                onPress={acceptFriendRequest}
-                >
-                <Text style={styles.actionButtonText}>Accept Friend Request</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                style={styles.actionButton}
-                onPress={declineFriendRequest}
-                >
-                <Text style={styles.actionButtonText}>Decline Friend Request</Text>
-                </TouchableOpacity>
+                {currentUserId === requesterId ? null : (
+                <>
+                    <TouchableOpacity
+                    style={styles.actionButton}
+                    onPress={acceptFriendRequest}
+                    >
+                    <Text style={styles.actionButtonText}>Accept Friend Request</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.actionButton}
+                    onPress={declineFriendRequest}
+                    >
+                    <Text style={styles.actionButtonText}>Decline Friend Request</Text>
+                    </TouchableOpacity>
+                </>
+                )}
             </View>
             )}
         </View>
         )}
+
 
         {relationshipStatus === 'friends' && (
         <TouchableOpacity

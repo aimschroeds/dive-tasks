38350ab24aeb404748a140e0ef1d1f6ac0d3ee30
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-03-08 22:13:28 +0100

    Removed old logging

--- views/LoginView.js
+++ views/LoginView.js
@@ -27,7 +27,6 @@ import {
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredentials) => {
           const user = userCredentials.user;
-          console.log(user);
         })
         .catch((error) => setErrorMessage(error.message));
     };

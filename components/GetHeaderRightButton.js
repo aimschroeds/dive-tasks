f0552212e8f075ba7eb427dcfc97cc2421dc7bb1
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-03-13 10:12:30 +0100

    Updated headerRight to show notifications as read/unread

--- components/GetHeaderRightButton.js
+++ components/GetHeaderRightButton.js
@@ -1,46 +1,88 @@
-// From: https://reactnavigation.org/docs/screen-options-resolution/#setting-parent-screen-options-based-on-child-navigators-state
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import { db, auth } from '../firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
-import { TouchableOpacity, Text } from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useNavigation } from '@react-navigation/native';
-import { useLayoutEffect, useState } from 'react';
 
+const getUnreadNotificationsCount = async (userId) => {
+  console.log('getUnreadNotificationsCount', userId)
+  const notificationsRef = collection(db, 'notifications');
+  const q = query(
+    notificationsRef,
+    where('recipientId', '==', userId),
+    where('read', '==', false),
+  );
+
+  const querySnapshot = await getDocs(q);
+  console.log("querySnapshot.size: ", querySnapshot.size)
+  return querySnapshot.size;
+};
+
+/**
+ * GetHeaderRightButton component for rendering the right header button based on the focused route
+ * @component
+ * @param {Object} props - Component properties
+ * @param {Object} props.route - Route object
+ * @returns {React.Node} - GetHeaderRightButton component
+ */
 const GetHeaderRightButton = (props) => {
-  const { route } = props;
+  const { route, user } = props;
   const navigation = useNavigation();
-  const [routeName, setRouteName] = useState(getFocusedRouteNameFromRoute(route) ?? 'Home');
+  const [unreadNotificationsCount, setUnreadNotificationsCount] = useState(0);
+  const [routeName, setRouteName] = useState(
+    getFocusedRouteNameFromRoute(route) ?? "Home"
+  );
 
   useLayoutEffect(() => {
     const currentRouteName = getFocusedRouteNameFromRoute(route);
     setRouteName(currentRouteName);
   }, [navigation, route]);
 
+  useEffect(() => {
+    const fetchUnreadNotificationsCount = async () => {
+      console.log("fetchUnreadNotificationsCount")
+      const count = await getUnreadNotificationsCount(user.uid);
+      setUnreadNotificationsCount(count);
+      console.log(count)
+    };
+
+    fetchUnreadNotificationsCount();
+  }, []);
+
+  /**
+   * Render the GetHeaderRightButton component
+   * @returns {React.Node} - The GetHeaderRightButton component
+   */
   return (
     <>
-    {
-      routeName == 'Plans' ? (
+      {routeName === "Plans" ? (
         <TouchableOpacity
-                    style={{ marginRight: 15 }}
-                    onPress={() => navigation.navigate("Plan Form")}
-                >
-                    <MaterialCommunityIcons name="plus-thick" color="#AA77FF" size={26} />
+          style={{ marginRight: 15 }}
+          onPress={() => navigation.navigate("Plan Form")}
+        >
+          <MaterialCommunityIcons
+            name="plus-thick"
+            color="#AA77FF"
+            size={26}
+          />
         </TouchableOpacity>
-      ) : 
-      routeName == 'Profile' ? (
+      ) : routeName === "Home" ? (
         <TouchableOpacity
-                    style={{ marginRight: 15 }}
-                    onPress={() => navigation.navigate("Edit Profile")}
-                >
-                    <MaterialCommunityIcons name="account-edit" color="#AA77FF" size={26} />
+          style={{ marginRight: 15 }}
+          onPress={() => navigation.navigate("Notifications")}
+        >
+          {unreadNotificationsCount > 0 ? (
+            <MaterialCommunityIcons name="bell" size={24} color="#AA77FF" />
+          ) : (
+            <MaterialCommunityIcons name="bell-outline" size={24} color="#AA77FF" />
+          )}
         </TouchableOpacity>
-      )
-      :
-      (
-        <Text> Home</Text>
-      )
-
-    }
+      ) : (
+        <Text></Text>
+      )}
     </>
   );
 };

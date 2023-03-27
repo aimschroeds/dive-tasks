adcdde8be99c8165808240260a2611f893a152cc
Aimee Schroeder <aimee.schroeder1990@gmail.com>
2023-03-27 03:09:44 -0400

    Helper unit testing

---
+++ __tests__/helpers/addToCalendarExpo.test.js
@@ -0,0 +1,52 @@
+import { addToCalendarExpo } from '../mocks/helpers/addToCalendarExpo';
+import * as Calendar from 'expo-calendar';
+import { Alert } from 'react-native';
+
+describe('addToCalendarExpo', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('adds event to calendar', async () => {
+    Calendar.requestCalendarPermissionsAsync.mockResolvedValue({ status: 'granted' });
+    Calendar.getCalendarsAsync.mockResolvedValue([{ source: { name: 'Default', id: '1' } }]);
+    Calendar.createCalendarAsync.mockResolvedValue('calendarId');
+    Calendar.createEventAsync.mockResolvedValue('eventId');
+
+    const title = 'Test Event';
+    const startDate = new Date('2023-05-01T12:00:00Z');
+    const endDate = new Date('2023-05-01T13:00:00Z');
+    const location = 'Test Location';
+
+    await addToCalendarExpo(title, startDate, endDate, location);
+
+    expect(Calendar.requestCalendarPermissionsAsync).toHaveBeenCalledTimes(1);
+    expect(Calendar.getCalendarsAsync).toHaveBeenCalledTimes(1);
+    expect(Calendar.createCalendarAsync).toHaveBeenCalledTimes(1);
+    expect(Calendar.createEventAsync).toHaveBeenCalledTimes(1);
+    expect(Alert.alert).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not add event if permission is not granted', async () => {
+    Calendar.requestCalendarPermissionsAsync.mockResolvedValue({ status: 'denied' });
+
+    await addToCalendarExpo('Test Event', new Date(), new Date(), 'Test Location');
+
+    expect(Calendar.requestCalendarPermissionsAsync).toHaveBeenCalledTimes(1);
+    expect(Alert.alert).toHaveBeenCalledTimes(1);
+    expect(Calendar.createCalendarAsync).toHaveBeenCalledTimes(0);
+    expect(Calendar.createEventAsync).toHaveBeenCalledTimes(0);
+  });
+
+  it('handles error while adding event', async () => {
+    Calendar.requestCalendarPermissionsAsync.mockResolvedValue({ status: 'granted' });
+    Calendar.getCalendarsAsync.mockRejectedValue(new Error('Error'));
+
+    await addToCalendarExpo('Test Event', new Date(), new Date(), 'Test Location');
+
+    expect(Calendar.requestCalendarPermissionsAsync).toHaveBeenCalledTimes(1);
+    expect(Calendar.getCalendarsAsync).toHaveBeenCalledTimes(1);
+    expect(Calendar.createCalendarAsync).toHaveBeenCalledTimes(0);
+    expect(Calendar.createEventAsync).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ __tests__/helpers/friendsHelpers.test.js
@@ -0,0 +1,64 @@
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import { getFriends } from "../../helpers/friendsHelpers";
+
+jest.mock("firebase/firestore", () => ({
+  getFirestore: jest.fn(),
+  collection: jest.fn(),
+  query: jest.fn(),
+  where: jest.fn(),
+  getDocs: jest.fn(),
+}));
+
+describe("getFriends function", () => {
+  it("should return an empty array if userId is not provided", async () => {
+    const friends = await getFriends();
+
+    expect(friends).toEqual([]);
+  });
+
+  it("should return an array of friend userIds", async () => {
+    const mockQueryResults1 = {
+      docs: [
+        { data: () => ({ userId2: "friend1" }) },
+        { data: () => ({ userId2: "friend2" }) },
+      ],
+    };
+    const mockQueryResults2 = {
+      docs: [
+        { data: () => ({ userId1: "friend3" }) },
+        { data: () => ({ userId1: "friend4" }) },
+      ],
+    };
+    const mockQueryResults3 = {
+      docs: [
+        { data: () => ({ userId2: "friend5" }) },
+      ],
+    };
+    const mockQueryResults4 = {
+      docs: [],
+    };
+    const expectedFriends = [
+      "friend1",
+      "friend2",
+      "friend3",
+      "friend4",
+      "friend5",
+    ];
+
+    getFirestore.mockReturnValue({});
+    collection.mockReturnValue({});
+    query.mockReturnValueOnce(mockQueryResults1);
+    query.mockReturnValueOnce(mockQueryResults2);
+    query.mockReturnValueOnce(mockQueryResults3);
+    query.mockReturnValueOnce(mockQueryResults4);
+
+    const friends = await getFriends("testUserId");
+
+    expect(friends).toEqual(expectedFriends);
+    expect(getFirestore).toHaveBeenCalledTimes(1);
+    expect(collection).toHaveBeenCalledWith({}, "relationships");
+    expect(query).toHaveBeenCalledTimes(4);
+    expect(where).toHaveBeenCalledTimes(4);
+    expect(getDocs).toHaveBeenCalledTimes(4);
+  });
+});
